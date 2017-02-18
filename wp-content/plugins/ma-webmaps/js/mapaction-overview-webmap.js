4d88bedf2f17e6be0c3642273a2b0cf1a06c68e9
Anthony Giles <ad_giles@hotmail.com>
2017-02-18 13:26:13 +0000

    Change initial start extent

--- wp-content/plugins/ma-webmaps/js/mapaction-overview-webmap.js
+++ wp-content/plugins/ma-webmaps/js/mapaction-overview-webmap.js
@@ -41,7 +41,7 @@
   esriConfig.defaults.io.corsEnabledServers.push("spreadsheets.google.com");
   var clusterLayer;
   var map = new Map("map", {
-    center: [40,0],
+    center: [60,0],
     zoom: 1,
     basemap: "gray-vector",
     wrapAround180: false,
